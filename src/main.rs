7b1235221288f7e925acb65d6b746362ea81ea41
Maksym Zhuk <maksymzhuk759@gmail.com>
2025-08-15 10:37:24 +0300

    add connect to database

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web::{self, get}, App, HttpResponse, HttpServer};
 use dotenvy::dotenv;
 use log::info;
+use sea_orm::Database;
 
 mod routes;
 mod handlers;
@@ -13,12 +14,18 @@ async  fn main() {
     env_logger::init();
 
     let port: u16 = env::var("PORT").unwrap().parse().unwrap();
+    
+    info!("Connected to database...");
+
+    let db = Database::connect(env::var("DATABASE_URL").unwrap()).await.expect("Failed to connect to database");
 
     info!("Server started in the port: {}", port);
 
     HttpServer::new(move || {
         App::new()
-            .configure(routes::init_routes)
+        .app_data(web::Data::new(db.clone()))
+        .configure(routes::init_routes)
+        
     })
     .bind(("0.0.0.0", port))
     .unwrap()
